f08a4cd87e66d5f573920d0e2d8b4ee30a37a0a4
Rakhmatulla <raxo00012345@gmail.com>
2021-04-19 15:57:56 +0600

    .

---
+++ src/main/java/kz/iitu/lms/security/Config.java
@@ -0,0 +1,46 @@
+package kz.iitu.lms.security;
+import kz.iitu.lms.service.impl.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class Config extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/swagger-ui/**").permitAll()
+                .antMatchers("/swagger").permitAll()
+                .antMatchers("/users/create").permitAll()
+//                .antMatchers("/users/create").hasAuthority("ADMIN")
+//                .antMatchers("/books/create").hasAuthority("ADMIN")
+                .antMatchers("/books/create").hasAuthority("READER")
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JwtTokenGeneratorFilter(authenticationManager()))
+                .addFilterAfter(new JwtTokenAuthFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
+}
